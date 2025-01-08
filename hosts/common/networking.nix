f1c3a7480204dfe3e1ab8ffc73f34d0941f78641
Mikolaj Bajtkiewicz <mikolaj.ba@pm.me>
2025-01-08 23:36:03 +0100

    refactor: remove all tangled files

--- hosts/common/networking.nix
+++
@@ -1,9 +0,0 @@
-# [[file:../../README.org::*Networking][Networking:1]]
-{ ... }:
-
-{
-  networking.networkmanager.enable = true;
-  networking.wireguard.enable = true;
-  networking.firewall.enable = true;
-}
-# Networking:1 ends here
