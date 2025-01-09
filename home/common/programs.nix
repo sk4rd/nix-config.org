0c36f14bc7ec680834e42d479354a8bc0a7366e4
Mikolaj Bajtkiewicz <mikolaj.ba@pm.me>
2025-01-09 23:55:48 +0100

    refactor: add simple programs config

---
+++ home/common/programs.nix
@@ -0,0 +1,24 @@
+{ ... }:
+
+{
+  # Imports of programs with larger configs
+  imports = [
+    ./emacs.nix
+    ./git.nix
+    ./hyprland.nix
+    ./kitty.nix
+    ./lf.nix
+  ];
+
+  # Programs with short or simple configs which are automatically enabled
+  programs = builtins.mapAttrs (name: value: { enable = true; }) {
+    bat = { };
+    btop = { };
+    imv = { };
+    mpv = { };
+    zathura = { };
+    direnv.nix-direnv.enable = true;
+    gpg.scdaemonSettings.disable-ccid = true; # Disable the integrated support for CCID compliant readers
+    ssh.extraConfig = "IdentityAgent /run/user/1000/gnupg/S.gpg-agent.ssh"; # Use the GPG agent for SSH authentication
+  };
+}
