f02f8c61b85b66d319121555afeaf60485f4dfc0
Mikolaj Bajtkiewicz <mikolaj.ba@pm.me>
2025-01-22 11:17:21 +0100

    refactor: change tofi styling

--- home/common/programs/tofi.nix
+++ home/common/programs/tofi.nix
@@ -4,10 +4,20 @@
   programs.tofi = {
     enable = true;
     settings = {
-      font = "IBM Plex Mono";
+      font = "BlexMono Nerd Font";
       font-size = "12";
       width = "100%";
-      height = "30px";
+      height = 20;
+      anchor = "bottom";
+      horizontal = true;
+      border-width = 0;
+      outline-width = 0;
+      padding-top = 0;
+      padding-bottom = 0;
+      padding-left = 10;
+      padding-right = 0;
+      result-spacing = 15;
+      min-input-width = 100;
     };
   };
 }
